a389ec6e04e1538bc0c76bec834d287bf249de09
Alvaro Ortiz <alvaro@smultron.org>
2019-09-13 14:54:48 +0200

    Moved paper to own folder

--- auto/paper.el
+++ auto/paper.el
@@ -3,15 +3,18 @@
  (lambda ()
    (LaTeX-add-bibitems
     "berners1998semantic"
+    "corcho2003methodologies"
     "di2006automatic"
     "kiniti2013wikis"
     "noy2001ontology"
     "ortiz2016"
     "patzschke2016"
+    "pieterse2014lists"
     "DBLP:journals/aimatters/Musen15"
     "rdfspec"
     "smw"
     "wikipediasemantic"
-    "oren2006semantic"))
+    "oren2006semantic"
+    "zhou2007ontology"))
  :bibtex)
 
--- paper/publication_status.md
+++ paper/publication_status.md
@@ -1,6 +1,8 @@
 Journal, submitted, rejected, tag
 JOSS, 05.09.2019, 07.09.2019, version 1.0
 
+Ubiquity, 13.09.2019,,version 1.1
+
+
 Possibilities:
 * https://www.digitalstudies.org
-* https://ubiquity.acm.org/index.cfm
