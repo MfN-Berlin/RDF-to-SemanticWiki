c8dc6a633242f6deb2ad331525daab39e554003c
Alvaro Ortiz <alvaro@smultron.org>
2019-09-26 16:12:27 +0200

    Revamping paper as case study

---
+++ paper/auto/paper.el
@@ -0,0 +1,32 @@
+(TeX-add-style-hook
+ "paper"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "IJCS_template"
+    "IJCS_template10")
+   (LaTeX-add-labels
+    "eq:jaa"
+    "theo1"
+    "lemma1"
+    "one"
+    "tab1"
+    "a1")
+   (LaTeX-add-bibitems
+    "braley"
+    "chhikara"
+    "gupta95a"
+    "gupta95b"
+    "gupta97"
+    "gurland94"
+    "gurland95"
+    "jorgensen"
+    "mills71"
+    "park99"
+    "tang"
+    "winkler"
+    "wong88"
+    "wong89"
+    "wong91"))
+ :latex)
+
