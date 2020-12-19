bec1e1458cd584e4b08707ba756b2c54029f21ac
Alvaro Ortiz-Troncoso <alvaro@smultron.org>
2020-12-19 22:50:40 +0100

    Templates fro ACM

--- paper/auto/paper.el
+++ paper/auto/paper.el
@@ -1,24 +1,17 @@
 (TeX-add-style-hook
  "paper"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("acmart" "manuscript" "screen" "review")))
    (TeX-run-style-hooks
     "latex2e"
-    "ijcs_template"
-    "ijcs_template10")
+    "acmart"
+    "acmart10")
+   (TeX-add-symbols
+    "BibTeX")
    (LaTeX-add-labels
     "tab1")
-   (LaTeX-add-bibitems
-    "berners1998"
-    "corcho2003"
-    "diiorio2006"
-    "gandon2014"
-    "kiniti2013"
-    "musen2015"
-    "noy2001"
-    "oren2006"
-    "ortiz2016"
-    "patzschke2016"
-    "pieterse2014"
-    "zhou2007"))
+   (LaTeX-add-bibliographies
+    "paper.bib"))
  :latex)
 
